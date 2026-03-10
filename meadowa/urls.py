d5510b18694ac30d04a5b89cf5280c079ea3ab8c
Sooryan <sooryanarayanan500@gmail.com>
2026-03-10 22:27:36 +0530

    Serve demo media files in production

--- meadowa/urls.py
+++ meadowa/urls.py
@@ -1,12 +1,13 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('web.urls',namespace='web')),
+    path("", include('web.urls', namespace='web')),
     path('ckeditor/', include('ckeditor_uploader.urls')),  # Add this line for CKEditor integration
-    path('cart/', include('web.urls',namespace='web')),
+    path('cart/', include('web.urls', namespace='web')),
     path('seller/', include('web.urls', namespace='web')),
-
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
